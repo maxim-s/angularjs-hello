ac8f17d7b569f84525101428692c32b952f334fa
maxim.salamatko <maxim.salamatko@fastdev.se>
2014-02-02 17:54:36 +0000

    refactoring request handling in the separate file

--- app/server.js
+++ app/server.js
@@ -7,114 +7,21 @@ var util = require('util'),
     events = require('events'),
     readline = require('readline'),
     stream = require('stream'),
-	qs = require('querystring'),
+	  qs = require('querystring'),
+    RequestHandler = require('./infrastructure/RequestHandler.js'),
     BooksRepository = require('./BooksRepository.js');
 
 var DEFAULT_PORT = 8000;
 
-var mimeMap = {
-  'txt': 'text/plain',
-  'html': 'text/html',
-  'css': 'text/css',
-  'xml': 'application/xml',
-  'json': 'application/json',
-  'js': 'application/javascript',
-  'jpg': 'image/jpeg',
-  'jpeg': 'image/jpeg',
-  'gif': 'image/gif',
-  'png': 'image/png',
-  'svg': 'image/svg+xml'
-};
-
-// Books Repository
-
 console.log(BooksRepository);
 
 var booksRepository = new BooksRepository(fs,readline,stream);
+var requestHandler = new RequestHandler(booksRepository,fs);
 
-booksRepository.addBook({ title : "book 1", author : "author 1" })
-booksRepository.addBook({ title : "book 2", author : "author 2" })
-
-
-// Books Repository end
-
-function handleNumbersRequest(req,res){
-	res.writeHead(200, {
-				'Content-Type': mimeMap.json
-	});
-
-	res.end(JSON.stringify([{first:"123"},{second:"456"}]));
-}
-
-function getPostObject(data){
-	for(var key in data){
-				return key;
-			};
-}
-
-function handleBooksRequest(req,res) {
-	var handled = false;
-	if (req.method == "POST") {
-		 var body = '';
-		req.on('data',function(data){
-			body += data;
-		});
-		 req.on('end', function () {
-		 	var book = JSON.parse(getPostObject(qs.parse(body)));
-		 	booksRepository.addBook(book);
-            //addBook(JSON.parse(getPostObject(qs.parse(body))));
-			handled = true;
-        });
-	}
-	if (req.method == "DELETE") {
-		console.log("delete" , req);
-	}
-	if (req.method == "GET") {
-		var books = booksRepository.getBooks();
-
-		console.log(books);
-		if (req.url == "/books") {
-			res.writeHead(200, {
-				'Content-Type': mimeMap.json
-			});
-			res.end(JSON.stringify(books));
-			handled = true;
-		}
-	}
-
-	return handled;
-}
-
-function handleRootUrl(req, res) {
-	if(req.url == "/") {
-		fs.readFile("index.html", function(err, text){
-		  res.setHeader("Content-Type", "text/html");
-		  res.end(text);
-		});
-
-		return true;
-	}
-
-	return false;
-}
-
-function handleStaticFile(req, res) {
-	var path = ('./' + req.url).replace('//','/').replace(/%(..)/g, function(match, hex){
-		return String.fromCharCode(parseInt(hex, 16));
-	});
-
-	fs.readFile(path, function(err, text){
-		res.writeHead(200, {
-			'Content-Type': mimeMap[path.split('.').pop()] || 'text/plain'
-		});
-
-		res.end(text);
-	});
-}
 
 function main() {
 	var server = http.createServer(function(req, res) {
-		var handlers = [handleRootUrl, handleBooksRequest, handleStaticFile];
+		var handlers = requestHandler.getHandlers();
 
 		for (var i = 0; i < handlers.length; i++) {
 			var isRequestHandled = handlers[i](req, res);
